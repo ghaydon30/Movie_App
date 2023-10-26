83ce8e718aa1723283652de692bcb9e93c9a2471
Gregory Haydon <110743586+ghaydon30@users.noreply.github.com>
2023-10-26 10:03:31 -0500

    fixed port for Heroku service

--- index.js
+++ index.js
@@ -277,8 +277,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', {session: false}), a
 //     console.log('Your app is listening on port 8080');
 // });
 
-
-// const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
     console.log('Listening on port ' + port);
 });
